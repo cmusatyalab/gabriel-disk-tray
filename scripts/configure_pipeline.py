212b4dc40a169395f4e94e4b457cdbbf81afd75a
Junjue Wang <junjuew@cs.cmu.edu>
2018-04-29 21:30:07 -0400

    add in dataset visualization and more customization for pipeline

--- scripts/configure_pipeline.py
+++ scripts/configure_pipeline.py
@@ -1,11 +1,9 @@
 import os
-import pprint
-import glob
 import re
 
 import fire
-from logzero import logger
 import tensorflow as tf
+from logzero import logger
 
 
 def get_tfrecord_data_num(file_path):
@@ -17,28 +15,37 @@ def get_tfrecord_data_num(file_path):
 
 def configure_pipeline(num_classes, learning_rate, max_step,
                        fine_tune_checkpoint, data_dir, template_path, min_dimension=600, max_dimension=1070,
-                       batch_size=5):
+                       batch_size=5, max_detections_per_class=100, max_total_detections=300):
     """Configure a pipeline based on template.
 
     The output of the pipeline will be saved to data_dir.
     """
     with open(template_path) as f:
         template = f.read()
-    template = re.sub('num_classes: [0-9]*', 'num_classes: {}'.format(num_classes), template)
-    template = re.sub('min_dimension: [0-9]*', 'min_dimension: {}'.format(min_dimension), template)
-    template = re.sub('max_dimension: [0-9]*', 'max_dimension: {}'.format(max_dimension), template)
-    template = re.sub('(?<=\s)batch_size: [0-9]*', 'batch_size: {}'.format(batch_size), template)
-    template = re.sub('initial_learning_rate: [0-9\.]*', 'initial_learning_rate: {:f}'.format(learning_rate),
+    template = re.sub('(?<=\s)num_classes: [0-9]+', 'num_classes: {}'.format(num_classes), template)
+    template = re.sub('(?<=\s)min_dimension: [0-9]+', 'min_dimension: {}'.format(min_dimension), template)
+    template = re.sub('(?<=\s)max_dimension: [0-9]+', 'max_dimension: {}'.format(max_dimension), template)
+    template = re.sub('(?<=\s)batch_size: [0-9]+', 'batch_size: {}'.format(batch_size), template)
+    template = re.sub('(?<=\s)max_detections_per_class: [0-9]+',
+                      'max_detections_per_class: {}'.format(max_detections_per_class), template)
+    template = re.sub('(?<=\s)max_total_detections: [0-9]*', 'max_total_detections: {}'.format(max_total_detections),
                       template)
-    template = re.sub('num_steps: [0-9]*', 'num_steps: {}'.format(max_step), template)
-    template = re.sub('fine_tune_checkpoint: ".*"',
+    template = re.sub('(?<=\s)learning_rate: [0-9\.]+', 'learning_rate: {:f}'.format(learning_rate),
+                      template)
+    template = re.sub('(?<=\s)num_steps: [0-9]+', 'num_steps: {}'.format(max_step), template)
+    template = re.sub('(?<=\s)fine_tune_checkpoint: ".*"',
                       'fine_tune_checkpoint: "{}"'.format(os.path.abspath(fine_tune_checkpoint)),
                       template)
     data_files = ['train.record', 'val.record', 'label_map.pbtxt']
     for data_file in data_files:
         template = re.sub(data_file, os.path.abspath(os.path.join(data_dir, data_file)), template)
     eval_data_file_path = os.path.join(data_dir, 'val.record')
-    template = re.sub('num_examples: [0-9]*', 'num_examples: {}'.format(get_tfrecord_data_num(eval_data_file_path)),
+    num_eval_data = get_tfrecord_data_num(eval_data_file_path)
+    template = re.sub('(?<=\s)num_examples: [0-9]+',
+                      'num_examples: {}'.format(num_eval_data),
+                      template)
+    template = re.sub('(?<=\s)num_visualizations: [0-9]+',
+                      'num_visualizations: {}'.format(num_eval_data),
                       template)
     output_file_path = os.path.join(data_dir, 'pipeline.config')
     with open(output_file_path, 'w') as f:
--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,9 +1,62 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import glob
+import io
 import os
 import pprint
-import glob
 
+import PIL.Image
+import PIL.ImageDraw
 import fire
 import tensorflow as tf
+from lxml import etree
+from object_detection.utils import dataset_util
+
+
+def read_image_file(file_path):
+    with tf.gfile.GFile(file_path, 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = PIL.Image.open(encoded_jpg_io)
+    if image.format != 'JPEG':
+        raise ValueError('Image format not JPEG')
+    return image
+
+
+def visualize_pascal_dataset(data_dir, output_dir):
+    image_subdirectory = ''
+    examples_path = os.path.join(data_dir, 'ImageSets', 'Main', 'train' + '.txt')
+    annotations_dir = os.path.join(data_dir, 'Annotations')
+    examples_list = dataset_util.read_examples_list(examples_path)
+    for idx, example in enumerate(examples_list):
+        path = os.path.join(annotations_dir, example + '.xml')
+        with tf.gfile.GFile(path, 'r') as fid:
+            xml_str = fid.read()
+            xml = etree.fromstring(xml_str)
+            data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+            img_path = os.path.join(data['folder'], image_subdirectory, data['filename'])
+            full_path = os.path.join(data_dir, img_path)
+            image = read_image_file(full_path)
+            width = int(data['size']['width'])
+            height = int(data['size']['height'])
+            assert width == image.size[0]
+            assert height == image.size[1]
+
+            if 'object' in data:
+                for obj in data['object']:
+                    xmin = float(obj['bndbox']['xmin'])
+                    ymin = float(obj['bndbox']['ymin'])
+                    xmax = float(obj['bndbox']['xmax'])
+                    ymax = float(obj['bndbox']['ymax'])
+                    classes_text = obj['name']
+                    draw = PIL.ImageDraw.Draw(image)
+                    draw.rectangle(((xmin, ymin), (xmax, ymax)), fill=None, outline='blue')
+                    draw.text((xmin, ymin), classes_text)
+                    del draw
+            image.save(os.path.join(output_dir, os.path.basename(img_path)), 'JPEG')
+            image.close()
 
 
 def read_tfrecord_file(file_path, debug=False):
@@ -25,11 +78,15 @@ def get_dataset_stats(dataset_dir):
     stats = {}
     for split, split_file_pattern in splits.items():
         split_files = glob.glob(os.path.join(imageset_dir, split_file_pattern))
+
         stats[split] = {}
         for data_file in split_files:
             with open(data_file) as f:
-                line_num = len(f.read().splitlines())
-            stats[split][os.path.basename(data_file)] = line_num
+                content = f.read().splitlines()
+                line_num = len(content)
+                # specific to TPOD exported Pascal Dataset
+                video_names = set(['_'.join(line.split('_')[:-1]) for line in content])
+            stats[split][os.path.basename(data_file)] = {'# examples': line_num, 'videos': video_names}
     pprint.pprint(stats)
 
 
